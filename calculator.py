8c2fa24173d26ca9000011e733329ac201025801
SarvarDev28 <sarvarbekismonjonov@gmail.com>
2025-09-15 01:31:37 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,15 @@
 # This is a test for GitHub
+
+operation = (input("Please, choose mathematic operation: +, -, *, /:  "))
+
+x1 = float(input("Please, enter your first number: "))
+x2 = float(input("Please, enter your second number: "))
+
+if operation == "+":
+    print( x1 + x2)
+if operation == "-":
+    print(x1 - x2)
+if operation == "*":
+    print(x1 * x2)
+if operation == "/":
+    print(x1 / x2)
