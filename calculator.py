0abf35428bb6c194f3c762abbe6e99f859881ab9
Sarvarbek <sarvarbekismonjonov@gmail.com>
2025-10-03 09:20:31 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,30 @@
-# This is a test for GitHub
+# This is a test for Github
 
-operation = (input("Please, choose mathematic operation: +, -, *, /:  "))
+operation = 0
+operations = ["+", "-", "*", "/"]
 
-x1 = float(input("Please, enter your first number: "))
-x2 = float(input("Please, enter your second number: "))
+while operation not in operations:
+    operation = (input("Please, choose mathematic operation: +, -, *, /:  "))
 
+x1 = (input("Please, enter your first number: "))
+while not x1.isdigit():
+    print("Faqat son kiriting!")
+    x1 = (input("Please, enter your first number: "))
+    exit
+x2 = (input("Please, enter your second number: "))
+while not x2.isdigit():
+    print("Faqat son kiriting!")
+    x2 = (input("Please, enter your second number: "))
+    exit
+
+x1 = int(x1)
+x2 = int(x2)
+    
 if operation == "+":
-    print( x1 + x2)
-if operation == "-":
+    print(x1 + x2)
+elif operation == "-":
     print(x1 - x2)
-if operation == "*":
+elif operation == "*":
     print(x1 * x2)
-if operation == "/":
+elif operation == "/":
     print(x1 / x2)
